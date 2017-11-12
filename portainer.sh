0ee30a87dde49170875ac02984fb7b4186fb9bfb
maciek01 <maciek@kolesnik.org>
2017-11-12 14:01:53 +0000

    docker stuff

--- portainer.sh
+++ portainer.sh
@@ -3,3 +3,9 @@ docker restart pi-portainer
 
 docker run --name pi-portainer --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
 
+
+docker restart pi-postgres
+
+docker run --name pi-postgres --restart always -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=pi -d -p 5432:5432 postgres
+
+
