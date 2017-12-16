5a9fe49ff3dfacb95a73eb94e5d9515cf8cb8330
Maciek Kolesnik <maciek@kolesnik.org>
2017-12-15 19:37:21 -0500

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -1,4 +1,11 @@
 
+# curl -sSL https://get.docker.com | sh
+
+# optional:
+# usermod -a -G docker pi
+
+
+
 docker restart pi-portainer
 
 docker run --name pi-portainer --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
